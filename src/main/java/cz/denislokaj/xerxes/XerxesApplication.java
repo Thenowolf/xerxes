6ac4662b76f98a11b1b294eebd5a74583cd5de87
Thenowolf <85835402+Thenowolf@users.noreply.github.com>
2025-03-27 19:49:04 +0100

    add spring initializr default project

---
+++ src/main/java/cz/denislokaj/xerxes/XerxesApplication.java
@@ -0,0 +1,13 @@
+package cz.denislokaj.xerxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XerxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XerxesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/denislokaj/xerxes/XerxesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.denislokaj.xerxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XerxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
